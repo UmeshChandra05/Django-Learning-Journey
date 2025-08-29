bd5bd8029a6fd68d0fe221b7f3583fe852739f8f
UmeshChandra05 <paruchuriumeshchandra@gmail.com>
2025-08-29 18:00:23 +0530

    Day 2: Passing dynamic data, IF & For loops
    - Passed dynamic data into HTML templates
    - Implemented IF statement in Django templates
    - Implemented For loop in Django templates

--- travello/models.py
+++ travello/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Destination:
+    id : int
+    name : str
+    desc : str
+    price : int
+    img : str
+    offer : bool
+    
+    def __init__(self, name, desc, price, img, offer):
+        self.name = name
+        self.desc = desc
+        self.price = price
+        self.img = img
+        self.offer = offer
--- travello/views.py
+++ travello/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-
+from .models import Destination
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    
+    dest1 = Destination('Mumbai', 'The City That I Never Visited', 900, 'destination_1.jpg', False)
+    dest2 = Destination('Hyderabad', 'The City I Was Borught Up', 800, 'destination_2.jpg', True)
+    dest3 = Destination('Chennai', 'The City That I Once Visited', 700, 'destination_3.jpg', False)
+    dests = [dest1, dest2, dest3]
+    return render(request, "index.html", {'dests': dests})
